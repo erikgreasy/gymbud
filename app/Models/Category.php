719076ae2559e806597fe1260e8615d48a1d2746
Erik Masny <erik.masny@gmail.com>
2023-08-27 15:24:43 +0200

    feat: order exercises and categories by name by default

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,6 +12,13 @@ class Category extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('order_by_name', function (Builder $builder) {
+            $builder->orderBy('name');
+        });
+    }
 
     public function exercises(): HasMany
     {
