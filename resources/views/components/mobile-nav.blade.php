938cdd044f263d2ba56dca8007d1bf3e9e589cfe
Erik Masny <erik.masny@gmail.com>
2023-08-29 21:39:53 +0200

    wip: set mobile nav padding safe area

--- resources/views/components/mobile-nav.blade.php
+++ resources/views/components/mobile-nav.blade.php
@@ -1,4 +1,4 @@
-<aside class="fixed bottom-0 w-full bg-gray-100 py-3">
+<aside id="mobile-nav" class="fixed bottom-0 w-full bg-gray-100 py-3">
     <nav class="container">
         <ul class="grid grid-cols-4 gap-5 justify-center">
             <x-mobile-nav-item route="home" :is-active="request()->routeIs('home')">
@@ -11,12 +11,12 @@
             <x-mobile-nav-item route="exercises.index" :is-active="request()->routeIs('exercises.*')">
                 <x-slot:icon>
                     <svg
-                            class="w-6 h-6 mx-auto"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
+                        class="w-6 h-6 mx-auto"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
                     >
                         <path stroke-linecap="round" stroke-linejoin="round"
                               d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"/>
