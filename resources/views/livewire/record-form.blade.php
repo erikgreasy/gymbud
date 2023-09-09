61bd60c0434666d271ac4f0ed169c61bc76ba05b
Erik Masny <erik.masny@gmail.com>
2023-09-09 22:48:30 +0200

    feat: add buttons to easily add and sub reps and weight

--- resources/views/livewire/record-form.blade.php
+++ resources/views/livewire/record-form.blade.php
@@ -19,13 +19,59 @@
 
     <div class="mb-4">
         <x-input-label>Weight:</x-input-label>
-        <x-text-input wire:model="weight" type="number" inputmode="numeric" step="any" class="block w-full"/>
+
+        <div class="flex items-center gap-x-3">
+            <button
+                x-on:click="$wire.weight -= 2"
+                type="button"
+                class="flex items-center justify-center w-10 h-10 bg-gray-400 text-white rounded-md"
+            >
+                -
+            </button>
+
+            <x-text-input
+                wire:model="weight"
+                type="number"
+                inputmode="numeric"
+                step="any"
+                class="flex-grow text-center"
+            />
+
+            <button
+                x-on:click="$wire.weight = Number($wire.weight) + 2"
+                type="button"
+                class="flex items-center justify-center w-10 h-10 bg-gray-400 text-white rounded-md"
+            >
+                +
+            </button>
+        </div>
+
         <x-input-error :messages="$errors->get('weight')"/>
     </div>
 
     <div class="mb-4">
         <x-input-label>Reps:</x-input-label>
-        <x-text-input wire:model="reps" type="number" inputmode="numeric" class="block w-full"/>
+
+        <div class="flex items-center gap-x-3">
+            <button
+                x-on:click="$wire.reps--"
+                type="button"
+                class="flex items-center justify-center w-10 h-10 bg-gray-400 text-white rounded-md"
+            >
+                -
+            </button>
+
+            <x-text-input wire:model="reps" type="number" inputmode="numeric" class="flex-grow text-center"/>
+
+            <button
+                x-on:click="$wire.reps++"
+                type="button"
+                class="flex items-center justify-center w-10 h-10 bg-gray-400 text-white rounded-md"
+            >
+                +
+            </button>
+        </div>
+
         <x-input-error :messages="$errors->get('reps')"/>
     </div>
 
