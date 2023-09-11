2ea9d3d0e4811dfed6411c76920eb0412feb1e58
Erik Masný <51275432+erikgreasy@users.noreply.github.com>
2023-09-11 17:07:30 +0200

    feat: add buttons to navigate to prev/next session (#15)

--- resources/views/sessions/show.blade.php
+++ resources/views/sessions/show.blade.php
@@ -1,5 +1,31 @@
 <x-layouts.app>
     <div>
+        <div class="flex items-center mb-5">
+            @if($prevSession)
+                <a
+                    href="{{ route('sessions.show', ['session' => $prevSession]) }}"
+                    class="w-10 h-10 inline-flex items-center justify-center bg-gray-400"
+                    wire:navigate
+                >
+                    <
+                </a>
+            @endif
+
+            <div class="flex-grow text-center">
+                {{ $session->date->translatedFormat('l, d. M Y') }}
+            </div>
+
+            @if($nextSession)
+                <a
+                    href="{{ route('sessions.show', ['session' => $nextSession]) }}"
+                    class="w-10 h-10 inline-flex items-center justify-center bg-gray-400"
+                    wire:navigate
+                >
+                    >
+                </a>
+            @endif
+        </div>
+
         <div class="mb-3">
             <x-button href="{{ route('sessions.records.create', ['session' => $session]) }}">
                 Add record
